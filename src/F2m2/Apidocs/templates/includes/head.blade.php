58737db689cf0bfa903ffd788e5ea4e9825013db
Hamlet <mhamlet@users.noreply.github.com>
2015-05-28 16:27:54 +0300

    Removing hardcoded API version in head templete

--- src/F2m2/Apidocs/templates/includes/head.blade.php
+++ src/F2m2/Apidocs/templates/includes/head.blade.php
@@ -10,7 +10,7 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="//code.jquery.com/ui/1.10.2/jquery-ui.min.js"></script>
-<script src="/assets/docs/api.v1/js/prettify.js"></script>
-<script src="/assets/docs/api.v1/js/waypoints.min.js"></script>
-<script src="/assets/docs/api.v1/js/highlight.js"></script>
-<script src="/assets/docs/api.v1/js/main.js"></script>
+<script src="/assets/docs/{prefix}/js/prettify.js"></script>
+<script src="/assets/docs/{prefix}/js/waypoints.min.js"></script>
+<script src="/assets/docs/{prefix}/js/highlight.js"></script>
+<script src="/assets/docs/{prefix}/js/main.js"></script>
